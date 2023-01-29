e7a58212d79e8ca3feab07e0c27214da163c10eb
johnathanvidu <johnathan.v@qualisystems.com>
2023-01-29 20:53:34 +0200

    Create variables.tf

---
+++ terraform/two_azure_vm_legacy_wi/variables.tf
@@ -0,0 +1,11 @@
+variable "vm_name" {
+  type = string
+  default = "vidovm"
+  description = "vm Name"
+}
+
+variable "resource_group" {
+  type = string
+  default = "vido_rg5"
+  description = "resource group name"
+}
