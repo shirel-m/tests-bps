b3dbae80c352ffc3db7283560cdc1623e66b329c
johnathanvidu <johnathan.v@qualisystems.com>
2023-01-29 21:09:27 +0200

    Update main.tf

--- terraform/two_azure_vm_legacy_wi/main.tf
+++ terraform/two_azure_vm_legacy_wi/main.tf
@@ -117,7 +117,7 @@ resource "azurerm_virtual_machine" "example2" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "myosdisk2"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
