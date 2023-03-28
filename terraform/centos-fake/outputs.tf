1e7872f9ec007a9c35b819f697a661e9126e2417
johnathanvidu <johnathan.v@qualisystems.com>
2023-03-28 20:25:12 +0300

    Create outputs.tf

---
+++ terraform/centos-fake/outputs.tf
@@ -0,0 +1,11 @@
+output "vsphere_virtual_machine_name" {
+  value = 'vido'
+}
+
+output "vsphere_virtual_machine_default_ip_address" {
+  value = '0.0.0.0'
+}
+
+output "qualix_link" {
+  value = 'http://vido.vido'
+}
