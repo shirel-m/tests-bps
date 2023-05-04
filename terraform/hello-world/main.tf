9d13f8a7d761de41649899a407253c93735ebd2b
johnathanvidu <johnathan.v@qualisystems.com>
2023-05-04 19:42:16 +0300

    Update main.tf

--- terraform/hello-world/main.tf
+++ terraform/hello-world/main.tf
@@ -5,6 +5,10 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
+resource "time_sleep" "wait_30_seconds" {
+  create_duration = "120s"
+}
+
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
   value = "Hello, World!"
