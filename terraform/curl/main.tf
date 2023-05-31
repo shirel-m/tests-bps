a4fe8db62eaca087c2a5892937b44cfc592de691
johnathanvidu <johnathan.v@qualisystems.com>
2023-05-31 17:59:07 +0300

    Update main.tf

--- terraform/curl/main.tf
+++ terraform/curl/main.tf
@@ -1,11 +1,21 @@
-provider "curl" {
+terraform {
+  required_providers {
+    curl = {
+      source = "marcofranssen/curl"
+      version = "0.2.2"
+    }
+  }
 }
 
-data "curl" "getTodos" {
+provider "curl" {
+  # Configuration options
+}
+
+data "curl_request" "getTodos" {
   http_method = "GET"
   uri = "https://jsonplaceholder.typicode.com/todos/1"
 }
 
 locals {
-  json_data = jsondecode(data.curl.getTodos.response)
+  json_data = jsondecode(data.curl_request.getTodos.response_body)
 }
